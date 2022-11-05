0e6f1b98b9201fb48c76ec4bf2f7f37b642acbe8
Mac <louis.niuniu@gmail.com>
2022-11-05 18:59:53 +0100

    Add a new function "login"
    update detail such as to change low character and to notice an empty list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -10,16 +11,16 @@ public class Main {
 
         ArrayList<ContactInformation> data = new ArrayList<>();
 
-//        data = InputFile.InputText();
+        data = InputFile.InputText();
 
-        ContactInformation ci1 = new ContactInformation("louis","han",23,"123456","malmo","lundsgatan","21188","12");
-        ContactInformation ci2 = new ContactInformation("hongwei","wang",45,"23456","stockholm","varvsgatan","1111","12");
-        ContactInformation ci3 = new ContactInformation("dog","cat",66,"123456","goteborg","lundsgatan","21188","12");
-        ContactInformation ci4 = new ContactInformation("dog","cat",66,"123456","louis","lundsgatan","21188","12");
-        data.add(ci1);
-        data.add(ci2);
-        data.add(ci3);
-        data.add(ci4);
+/*        ContactInformation ci1 = new ContactInformation("hongwei","han",23,"123456","malmo","lundsgatan","21188","12");
+        ContactInformation ci2 = new ContactInformation("louis","wang",45,"23456","stockholm","varvsgatan","1111","12");
+        ContactInformation ci3 = new ContactInformation("tiger","cat",66,"123456","goteborg","lundsgatan","21188","12");
+        ContactInformation ci4 = new ContactInformation("dog","cat",99,"123456","louis","lundsgatan","21188","12");
+
+
+        Collections.addAll(data, ci1, ci2, ci3, ci4);
+        */
 
 
 
@@ -34,13 +35,14 @@ public class Main {
             System.out.println("3. quit");
             switch (sc.nextLine()){
                 case "1" -> {
-                    GuestUsers gu = new GuestUsers();
-                    gu.questions(data);
+
+                    GuestUsers gu = new GuestUsers(data);
+                    gu.questions();
 
                 }
                 case "2" -> {
-                    Administrator adm = new Administrator();
-                    adm.questions(data);
+                    Login.login(data);
+
                 }
                 case "3" ->{
                     OutputFile.outputTextFile(data);
